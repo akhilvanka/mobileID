67fd7df2017015f0bdfbadc19c0d7d72180bbe0b
Akhil Vanka <dev+github@akhilv.me>
2024-02-17 13:23:17 -0500

    Initial Commit

---
+++ MobileID/MobileIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileIDApp.swift
+//  MobileID
+//
+//  Created by Akhil Vanka on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MobileIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
